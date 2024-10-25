641e12ff939b4e3f3ba6a46cedf4a84ad9de9073
Daniel Fox Franke <dfoxfranke@gmail.com>
2024-10-25 11:49:30 -0400

    [bedquilt] Init at 0.0.0

---
+++ game-crates/bedquilt/src/lib.rs
@@ -0,0 +1,6 @@
+//! This crate will eventually become a "batteries included" metacrate for the
+//! [Bedquilt project](https://bedquilt.io). Right now it's just a re-export of
+//! [bedquilt-io](bedquilt_io).
+#![no_std]
+
+pub use bedquilt_io;
